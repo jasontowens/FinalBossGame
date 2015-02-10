61aa9105651e824c38b65c7af02d040a1d87d2ec
Kyle Kyrazis <kkyrazis@ufl.edu>
2015-02-10 17:36:04 -0500

    Adding place holder classes to set up our project folder hierarchy

---
+++ FinalBossGame/test/modelTest/PlaceHolder.java
@@ -0,0 +1,14 @@
+package modelTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PlaceHolder {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

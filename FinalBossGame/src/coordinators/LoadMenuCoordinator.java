303b8126b10fd2fb61fd720065a7a724849b9152
mickeyjk <micohen13@gmail.com>
2015-02-12 21:35:23 -0500

    Added LoadMenuCoordinator, mods to other coords
    Added LoadMenuCoordinator, modified MainMenuCoordinator and
    PauseMenuCoordinator

---
+++ FinalBossGame/src/coordinators/LoadMenuCoordinator.java
@@ -0,0 +1,54 @@
+package coordinators;
+
+import java.util.ArrayList;
+
+public class LoadMenuCoordinator {
+	
+	/*--------------------- OTHER DATA MEMBERS ---------------------*/
+	private static CoordinatorScheduler scheduler = CoordinatorScheduler.getInstance();
+	
+	//Singleton Instance
+	private static LoadMenuCoordinator loadMenuCoordinator = null;
+	
+	/*--------------------- MENU STATUS ---------------------*/
+	private static int currentFileIndex = 0;
+	private static String currentFile = "";
+	private static ArrayList<String> saveFiles;
+	
+	
+	/*--------------------- CONSTRUCTORS ---------------------*/
+	private LoadMenuCoordinator(){
+		currentFile = saveFiles.get(currentFileIndex);
+	}
+	
+	
+	/*--------------------- LOAD COMMANDS ---------------------*/
+	public void confirmSelection(){
+		//TODO: Send selected filename to appropriate interface, change coordinator to game
+	}
+	
+	public void nextSelection(){
+		currentFileIndex = (currentFileIndex + 1) % saveFiles.size(); //increment index by 1 while keeping within list size indices
+		currentFile = saveFiles.get(currentFileIndex);
+	}
+	
+	public void previousSelection(){
+		currentFileIndex = (currentFileIndex - 1) % saveFiles.size(); //decrement index by 1 while keeping within list size indices
+		currentFile = saveFiles.get(currentFileIndex);
+	}
+	
+	
+	
+	/*--------------------- OTHER METHODS ---------------------*/
+	public void backToMainMenu(){
+		scheduler.changeCoordinator(CoordinatorType.MAIN_MENU);
+	}
+	
+	/*--------------------- SINGLETON METHODS ---------------------*/
+	public static LoadMenuCoordinator getInstance() {
+		if(loadMenuCoordinator == null) {
+			loadMenuCoordinator = new LoadMenuCoordinator();
+		}
+		return loadMenuCoordinator;
+	}
+}

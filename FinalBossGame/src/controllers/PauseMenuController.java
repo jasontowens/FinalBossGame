bb665ce2e624068444f86d4f3341ba775e0a7c64
mickeyjk <micohen13@gmail.com>
2015-02-12 22:15:24 -0500

    Connected controllers to coordinators
    Connected Pause, Load, and Menu controllers to their respective
    coordinators.
    Added unpause capabilities to the PauseMenuCoordinator

--- FinalBossGame/src/controllers/PauseMenuController.java
+++ FinalBossGame/src/controllers/PauseMenuController.java
@@ -1,13 +1,25 @@
 package controllers;
 
+import coordinators.PauseMenuCoordinator;
+
 public class PauseMenuController extends SceneController {
 
 	private static PauseMenuController pauseMenuController = null;
 	
-	public void useKey2() {}
-	public void useKey8() {}
-	public void useKeyEnter() {}
-	public void useKeyEscape() {}
+	private static PauseMenuCoordinator pauseReceiver = PauseMenuCoordinator.getInstance();
+	
+	public void useKey2() {
+		pauseReceiver.nextSelection();
+	}
+	public void useKey8() {
+		pauseReceiver.previousSelection();
+	}
+	public void useKeyEnter() {
+		pauseReceiver.confirmSelection();
+	}
+	public void useKeyEscape() {
+		pauseReceiver.unPause();
+	}
 	
 	public static PauseMenuController getInstance() {
 		if(pauseMenuController == null) {

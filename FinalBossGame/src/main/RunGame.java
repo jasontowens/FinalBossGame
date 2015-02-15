66e3915c1f22693c46f2991fd7989cda953d9aea
Matthew Kroeze <matthew.kroeze@gmail.com>
2015-02-15 13:26:13 -0500

    changes to main, fixed terrain constructor

--- FinalBossGame/src/main/RunGame.java
+++ FinalBossGame/src/main/RunGame.java
@@ -1,12 +1,16 @@
 package main;
 
+import entity.*;
+import map.*;
+
+
 /**
  *
  * @author moco
  */
 public class RunGame {
     
-    public static final int NUM_OF_TILES_WIDE = 21, NUM_OF_TILES_HIGH = 17;
+    public static final int NUM_OF_TILES_WIDE = 5, NUM_OF_TILES_HIGH = 5;
     public static final int TILE_WIDTH = 32, TILE_HEIGHT = 32; 
     public static final int WIDTH = NUM_OF_TILES_WIDE * TILE_WIDTH, HEIGHT= NUM_OF_TILES_HIGH * TILE_HEIGHT; //The width and height of the game window
     public static final int FPS = 60;//The prefered FPS animation rate
@@ -14,25 +18,18 @@ public class RunGame {
     
     public static void main(String[] args) {
         System.out.println("Game Window Size: " + WIDTH + "x" + HEIGHT);
-    	//MODEL SETUP
-    	
-    	//TODO: CREATE MODEL, store avatar in myAvatar and active mao in myMap
-    	
-    	//CONTROLLER SETUP
-    	/*InputParser myParser = new InputParser();
+    	GameMap myMap = new GameMap();
+    	for(int i = 0; i != 25; ++i){
+    		GameMap.addTile(new Tile(new Terrain("grass","a grassy tile","grass.png", GROUND)));
+    	}
+    	InputStream stream = new FileInputStream("./resources/saves/save1.xml");
+    	ObjectFactory f = new ObjectFactory(stream, myMap);
+    	Entity myAvatar = f.parseFile();
+    	InputParser myParser = new InputParser();
     	GameCoordinator myGameCoord = GameCoordiator.getInstance();
     	myGameCoord.setAvatar(myAvater);
-    	myGameCoord.setActiveMap(myMap);*/
-    	
-    	
-    	//
-    	//TODO: add the view:
     	GameWindow window =	new GameWindow();
-    	//window.setModel( para1, para2...)
-    	
-    	
-    	//To end game:
-    	//RUNNING = false;
+    	window.setModel(myGameCoord, )
     }
     
 }

899023c08f876e0a1becae51a92bf937980a0c63
Matthew Kroeze <matthew.kroeze@gmail.com>
2015-02-15 13:35:03 -0500

    added to main

--- FinalBossGame/src/main/RunGame.java
+++ FinalBossGame/src/main/RunGame.java
@@ -18,39 +18,24 @@ public class RunGame {
     public static boolean RUNNING = true;
     
     public static void main(String[] args) {
-        System.out.println("Game Window Size: " + WIDTH + "x" + HEIGHT);
-    	GameMap myMap = new GameMap();
-    	for(int i = 0; i != 25; ++i){
+        System.out.println("Game Window Size: " + WIDTH + "x" + HEIGHT); //initial message
+    	GameMap myMap = new GameMap();	//create empty game map
+    	for(int i = 0; i != 25; ++i){	//add tiles to game map
     		GameMap.addTile(new Tile(new Terrain("grass","a grassy tile","grass.png", GROUND)));
     	}
-    	InputStream stream = new FileInputStream("./resources/saves/save1.xml");
-    	ObjectFactory f = new ObjectFactory(stream, myMap);
+    	InputStream stream = new FileInputStream("./resources/saves/save1.xml"); //create stream with file to be loaded
+    	//initialize items and entities in the map
+    	ObjectFactory f = new ObjectFactory(stream, myMap);	
     	Entity myAvatar = f.parseFile();
+    	//set up controllers and coordinators
     	InputParser myParser = new InputParser();
     	GameCoordinator myGameCoord = GameCoordiator.getInstance();
     	myGameCoord.setAvatar(myAvater);
+    	myGameCoord.setActiveMap(myMap);
+    	//set up view
     	GameWindow window =	new GameWindow();
-    	window.setModel(myGameCoord, )
-    	//MODEL SETUP
-    	
-    	//TODO: CREATE MODEL, store avatar in myAvatar and active mao in myMap
-    	
-    	//CONTROLLER SETUP
-    	InputParser myParser = new InputParser();
-    	GameCoordinator myGameCoord = GameCoordinator.getInstance();
-    	//myGameCoord.setAvatar(myAvater);
-    	//myGameCoord.setActiveMap(myMap);
-    	
-    	
-    	//
-    	//TODO: add the view:
-    	GameWindow window =	new GameWindow();
+    	window.setModel(myGameCoord, MenuCoordinator.getInstance(), InventoryCoordinator.getInstance());
         window.addKeyListener(myParser);
-    	//window.setModel( para1, para2...)
-    	
-    	
-    	//To end game:
-    	//RUNNING = false;
     }
     
 }

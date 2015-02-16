e0810b0030fb5744ae81633c58e107cbef8edd70
almcaeobtac <almcaeobtac@gmail.com>
2015-02-16 01:59:14 -0500

    Entities show up!?
    Yes

--- FinalBossGame/src/viewport/MapViewPort.java
+++ FinalBossGame/src/viewport/MapViewPort.java
@@ -1,11 +1,19 @@
 package viewport;
 
 import coordinators.GameCoordinator;
+import gameobject.GameObject;
+import item.Item;
+
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+
+import entity.Entity;
 import main.RunGame;
+import map.CoordinatePair;
 import map.GameMap;
 import scene.Scene;
 
@@ -32,13 +40,24 @@ public class MapViewPort extends ViewPort {
         g.setFont(new Font("TimesRoman", Font.PLAIN, 20));
         g.drawString("MAP VIEWPORT", RunGame.WIDTH/2 - 85, RunGame.HEIGHT/2);
         
+        GameMap cmap = GameMap.getInstance();
         BufferedImage[][] images = RunGame.ml.getMap();
         for(int i = 0; i < RunGame.ml.getMapWidth(); i++){
             for(int j = 0; j < RunGame.ml.getMapHeight(); j++){
                 g.drawImage(images[j][i], i*RunGame.TILE_WIDTH, j*RunGame.TILE_HEIGHT, null);
+         
+                Item itemat = cmap.getItemAt(new CoordinatePair(i,j));
+                if(itemat != null)
+                {
+                	g.drawImage(RunGame.ssh.getSprite(((GameObject) itemat).getID()), i*RunGame.TILE_WIDTH, j*RunGame.TILE_HEIGHT, null);
+                }
+                
+                Entity entat = cmap.getEntityAt(new CoordinatePair(i,j));
+                if(entat != null)
+                {
+                	g.drawImage(RunGame.ssh.getSprite(entat.getID()), i*RunGame.TILE_WIDTH, j*RunGame.TILE_HEIGHT, null);
+                }
             }
-        }
-        
-        
+        }     
     }
 }

e15aa0444647033c489bbe603dbbd1efb18fbca3
Chris Moscoso <christophermoscoso@gmail.com>
2015-02-17 00:48:12 -0500

    Optimized ElementValidator class: Used to be 83 lines of code, now just 33 lines of code.

--- FinalBossGame/src/factories/ElementValidator.java
+++ FinalBossGame/src/factories/ElementValidator.java
@@ -1,59 +1,32 @@
 package factories;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 import org.w3c.dom.Element;
 
 public class ElementValidator {
-	public boolean ValidElement(Element e, String type) {
-		switch(type) {
-			case "entity":
-				if(e.getAttribute("occupation").length() < 1) return false;
-				if(e.getAttribute("x").length() < 1) return false;
-				if(e.getAttribute("y").length() < 1) return false;
-				return true;
-			case "takeableItem":
-				if(e.getAttribute("name").length() < 1) return false;
-				if(e.getAttribute("durability").length() < 1) return false;
-				return true;
-			case "stats":
-				if(e.getAttribute("livesleft").length() < 1) return false;
-				if(e.getAttribute("strength").length() < 1) return false;
-				if(e.getAttribute("agility").length() < 1) return false;
-				if(e.getAttribute("intellect").length() < 1) return false;
-				if(e.getAttribute("hardiness").length() < 1) return false;
-				if(e.getAttribute("experience").length() < 1) return false;
-				if(e.getAttribute("movement").length() < 1) return false;
-				if(e.getAttribute("hpcurrent").length() < 1) return false;
-				if(e.getAttribute("mpcurrent").length() < 1) return false;
-				if(e.getAttribute("defense").length() < 1) return false;
-				if(e.getAttribute("offense").length() < 1) return false;
-				return true;	
-			case "inventory":
-				if(e.getAttribute("name").length() < 1) return false;
-				if(e.getAttribute("durability").length() < 1) return false;
-				return true;
-			case "takeable":
-				if(e.getAttribute("name").length() < 1) return false;
-				if(e.getAttribute("x").length() < 1) return false;
-				if(e.getAttribute("y").length() < 1) return false;
-				if(e.getAttribute("durability").length() < 1) return false;
-				return true;
-			case "oneshot":
-				if(e.getAttribute("name").length() < 1) return false;
-				if(e.getAttribute("x").length() < 1) return false;
-				if(e.getAttribute("y").length() < 1) return false;
-				return true;
-			case "interactive":
-				if(e.getAttribute("name").length() < 1) return false;
-				if(e.getAttribute("x").length() < 1) return false;
-				if(e.getAttribute("y").length() < 1) return false;
-				return true;
-			case "obstacle":
-				if(e.getAttribute("name").length() < 1) return false;
-				if(e.getAttribute("x").length() < 1) return false;
-				if(e.getAttribute("y").length() < 1) return false;
-				return true;
-			default:
-				return false;
-		}
-	}
+
+    HashMap<String, List<String>> attributes = new HashMap();
+
+    public ElementValidator() {
+        attributes.put("entity", Arrays.asList("occupation", "x", "y"));
+        attributes.put("takeableItem", Arrays.asList("name", "durability"));
+        attributes.put("stats", Arrays.asList("stats", "livesleft", "strength", "agility, intellect", "hardiness", "experience", "movement", "hpcurrent", "mpcurrent", "defense", "offense"));
+        attributes.put("entity", Arrays.asList("inventory", "name", "durability"));
+        attributes.put("entity", Arrays.asList("takeable", "x", "y", "durability"));
+        attributes.put("entity", Arrays.asList("oneshot", "name", "x", "y"));
+        attributes.put("entity", Arrays.asList("interactive", "name", "x", "y"));
+        attributes.put("entity", Arrays.asList("obstacle", "name", "x", "y"));
+    }
+
+    public boolean ValidElement(Element e, String type) {
+
+        for (String attribute : attributes.get(type)) {
+            if (e.getAttribute(attribute).length() < 1) {
+                return false;
+            }
+        }
+        return true;
+    }
 }

209f6d3200a11a2d4e4077b441f4a7b8dd13518d
Chris Moscoso <christophermoscoso@gmail.com>
2015-02-17 01:20:19 -0500

    Optimized ElementValidator class: Used to be 83 lines of code, now just 33 lines of code. (Had to commit twice to fix mistyping)

--- FinalBossGame/src/factories/ElementValidator.java
+++ FinalBossGame/src/factories/ElementValidator.java
@@ -13,15 +13,14 @@ public class ElementValidator {
         attributes.put("entity", Arrays.asList("occupation", "x", "y"));
         attributes.put("takeableItem", Arrays.asList("name", "durability"));
         attributes.put("stats", Arrays.asList("stats", "livesleft", "strength", "agility, intellect", "hardiness", "experience", "movement", "hpcurrent", "mpcurrent", "defense", "offense"));
-        attributes.put("entity", Arrays.asList("inventory", "name", "durability"));
-        attributes.put("entity", Arrays.asList("takeable", "x", "y", "durability"));
-        attributes.put("entity", Arrays.asList("oneshot", "name", "x", "y"));
-        attributes.put("entity", Arrays.asList("interactive", "name", "x", "y"));
-        attributes.put("entity", Arrays.asList("obstacle", "name", "x", "y"));
+        attributes.put("inventory", Arrays.asList("name", "durability"));
+        attributes.put("takeable", Arrays.asList("x", "y", "durability"));
+        attributes.put("oneshot", Arrays.asList("name", "x", "y"));
+        attributes.put("interactive", Arrays.asList("name", "x", "y"));
+        attributes.put("obstacle", Arrays.asList("name", "x", "y"));
     }
 
     public boolean ValidElement(Element e, String type) {
-
         for (String attribute : attributes.get(type)) {
             if (e.getAttribute(attribute).length() < 1) {
                 return false;
